f269f4f435ee7ff90e39a430dd277e0dac4bbfd1
Przemek Marciniak <przemek.marciniak@racebets.com>
2018-12-11 10:38:49 +0100

    additional tests were deleted 2

--- test/test-day02.js
+++ test/test-day02.js
@@ -1,30 +1,30 @@
-/* eslint-env node, mocha */
-'use strict';
+// /* eslint-env node, mocha */
+// 'use strict';
 
-const assert = require('assert'),
-    day2part1 = require('../day02/2-1.js'),
-    day2part2 = require('../day02/2-2.js');
+// const assert = require('assert'),
+//     day2part1 = require('../day02/2-1.js'),
+//     day2part2 = require('../day02/2-2.js');
 
-describe('Day 2', () => {
+// describe('Day 2', () => {
 
-    describe('Part One', () => {
+//     describe('Part One', () => {
 
-        const test = '5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8';
+//         const test = '5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8';
 
-        it('should return checksum 8 + 4 + 6 = 18', () => {
-            assert.equal(18, day2part1(test));
-        });
+//         it('should return checksum 8 + 4 + 6 = 18', () => {
+//             assert.equal(18, day2part1(test));
+//         });
 
-    });
+//     });
 
-    describe('Part Two', () => {
+//     describe('Part Two', () => {
 
-        const test2 = '5\t9\t2\t8\n9\t4\t7\t3\n3\t8\t6\t5';
+//         const test2 = '5\t9\t2\t8\n9\t4\t7\t3\n3\t8\t6\t5';
 
-        it('should return checksum 4 + 3 + 2 = 9', () => {
-            assert.equal(9, day2part2(test2));
-        });
+//         it('should return checksum 4 + 3 + 2 = 9', () => {
+//             assert.equal(9, day2part2(test2));
+//         });
 
-    });
+//     });
 
-});
+// });
